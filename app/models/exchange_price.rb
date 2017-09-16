64afb3ee2600f6268f4cf890dceb3656667b6292
Gianluca Cinellu <cinellu@fd.nl>
2017-09-16 17:21:01 +0200

    adding test coverage and validation for exchange prices

--- app/models/exchange_price.rb
+++ app/models/exchange_price.rb
@@ -1,2 +1,9 @@
 class ExchangePrice < ApplicationRecord
+  @allowed_exchanges = ['Coinbase']
+  @allowed_coins = ['BTC']
+  @allowed_currencies = ['EUR', 'USD']
+
+  validates :exchange, inclusion: { in: @allowed_exchanges }
+  validates :coin, inclusion: { in: @allowed_coins }
+  validates :currency, inclusion: { in: @allowed_currencies }
 end
