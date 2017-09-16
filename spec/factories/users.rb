a9c023010b862b5a0e7f2f5b903fa5eed75779ca
Gianluca Cinellu <cinellu@fd.nl>
2017-09-17 00:14:27 +0200

    chore(creating_predictions): better formatting in user factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryGirl.define do
   factory :user do
     factory :user_valid do
-      email 'clementine@popsicle.com'
+      email    'clementine@popsicle.com'
       username 'Heavy Trader'
       password 'popsicles'
     end
 
     factory :user_no_username do
-      email 'clementine@popsicle.com'
+      email    'clementine@popsicle.com'
       password 'popsicles'
     end
 
@@ -17,7 +17,7 @@ FactoryGirl.define do
     end
 
     factory :user_no_password do
-      email 'clementine@popsicle.com'
+      email    'clementine@popsicle.com'
       username 'Heavy Trader'
     end
   end
