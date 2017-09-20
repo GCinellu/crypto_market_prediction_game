4d53e37600633429c7f02cb12856d7b7071cbb54
Gianluca Cinellu <cinellu@fd.nl>
2017-09-21 01:02:43 +0200

    chore(creating_predictions): adding routes for resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post '/authentication/signup', to: 'authentication#signup', as: :signup
 
   resources :predictions, only: [:index, :show, :create, :destroy]
+  resources :predicteds,  only: [:index, :show]
 
   root to: 'public#index'
 end
