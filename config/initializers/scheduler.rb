05bbb5c6436cc981411400e15d1d18d88879f744
Gianluca Cinellu <cinellu@fd.nl>
2017-09-21 01:02:03 +0200

    chore(creating_predictions): wip optimising scheduler's performances

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -3,6 +3,9 @@ require 'rufus-scheduler'
 s = Rufus::Scheduler.singleton
 
 s.every '30s' do
-  BtcCoinbasePrices.perform_now {}
-  ProcessPredictionsJob.perform_now {}
+  BtcCoinbasePrices.perform_now
+end
+
+s.every '1m' do
+  ProcessPredictionsJob.perform_now
 end
