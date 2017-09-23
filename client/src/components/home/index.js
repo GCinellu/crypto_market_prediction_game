9fe302c2d5aa508fd29875bd40fda74ee5c3fe20
Gianluca Cinellu <cinellu@fd.nl>
2017-09-23 22:51:31 +0200

    feature(core_client_auth): home component cleanup

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -9,26 +9,11 @@ import SubscribeNow from '../headers/subscribe-now';
 
 if(process.env.WEBPACK) require('./index.scss');
 
-const Home = ({ dispatch, todos }) => (
+const Home = () => (
   <div>
     <TopNav title='Oh Jeez Man' />
     <SubscribeNow />
-
-    <br />
-    {todos.map((todo) => (
-      <div key={ todo.id }>
-        <span style={ (todo.checked) ? { textDecoration: 'line-through' } : {} }>{ todo.text } </span>
-        <button onClick={() => dispatch(toggleTodo(todo.id))}>Toggle</button>
-      </div>
-    ))}
-    <br/>
-    <Link to='/page'>
-      <button>Go to page</button>
-    </Link>
   </div>
 );
 
-export default connect((state) => {
-  const { todos } = state;
-  return { todos };
-})(Home);
+export default Home;
